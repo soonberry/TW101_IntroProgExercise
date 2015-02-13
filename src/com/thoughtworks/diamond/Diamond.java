0dfcb94cd03729e25b68cec3a97c574fcdcba48a
soonberry <shuwenli89@126.com>
2015-02-14 01:18:22 +0800

    Diamond Exercise

---
+++ src/com/thoughtworks/diamond/Diamond.java
@@ -0,0 +1,32 @@
+package com.thoughtworks.diamond;
+
+/**
+ * Created by shuwenli on 15-2-14.
+ */
+public class Diamond {
+    public String drawLine(String c,int n){
+        String result="";
+        for (int i = 0; i < n; i++) {
+            result+=c;
+        }
+        return result;
+    }
+
+    public String drawIsoTriangle(int n){
+        String result="";
+        for (int i = 0; i <n; i++) {
+            result+=drawLine(" ",n-i-1)+drawLine("*",2*(i+1)-1)+"\n";
+        }
+        return result;
+    }
+
+
+    public String drawDiamond(int n) {
+        String result="";
+        result=drawIsoTriangle(3);
+        for (int i = n-1; i >0 ; i--) {
+            result+=drawLine(" ",n-i)+drawLine("*",2*i-1)+"\n";
+        }
+        return result;
+    }
+}
---
+++ test/com/thoughtworks/diamond/DiamondTest.java
@@ -0,0 +1,23 @@
+package com.thoughtworks.diamond;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DiamondTest {
+
+    @Test
+    public void testDrawIsoTriangle() throws Exception {
+        Diamond diamond=new Diamond();
+
+        assertEquals("  *\n ***\n*****\n",diamond.drawIsoTriangle(3));
+
+    }
+
+    @Test
+    public void testDrawDiamond() throws Exception {
+        Diamond diamond=new Diamond();
+
+        assertEquals("  *\n ***\n*****\n ***\n  *\n",diamond.drawDiamond(3));
+    }
+}
