c7dd932382f615448984ae5ee9e474094b709658
soonberry <shuwenli89@126.com>
2015-02-16 16:52:28 +0800

    Prime Factors Exercise 1.2

--- test/com/thoughtworks/primefactors/PrimeFactorsTest.java
+++ test/com/thoughtworks/primefactors/PrimeFactorsTest.java
@@ -11,11 +11,16 @@ public class PrimeFactorsTest {
     @Test
     public void testGenerate() throws Exception {
         PrimeFactors primeFactors=new PrimeFactors();
-        List<Integer> list=new ArrayList<Integer>();
-        list.add(2);
+        List<Integer> list1=new ArrayList<Integer>();
+        list1.add(2);
+        List<Integer> list2=new ArrayList<Integer>();
+        list2.add(2);
+        list2.add(3);
+        list2.add(5);
 
-        assertEquals(null,primeFactors.generate(1));
-        assertEquals(list,primeFactors.generate(2));
+        assertEquals(null, primeFactors.generate(1));
+        assertEquals(list1,primeFactors.generate(2));
+        assertEquals(list2,primeFactors.generate(30));
 
     }
 }
