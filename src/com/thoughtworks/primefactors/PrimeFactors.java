a277b8bbd3d261b159d3d0a3d95bf47f50052048
soonberry <shuwenli89@126.com>
2015-02-16 16:47:54 +0800

    Prime Factors Exercise 1.1

--- src/com/thoughtworks/primefactors/PrimeFactors.java
+++ src/com/thoughtworks/primefactors/PrimeFactors.java
@@ -1,13 +1,28 @@
 package com.thoughtworks.primefactors;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * Created by shuwenli on 15-2-16.
  */
 public class PrimeFactors {
     public List<Integer> generate(int n) {
-
-        return null;
+        List<Integer> factors=new ArrayList<Integer>();
+        if(n<2)
+            return null;
+        else{
+            for (int i = 2; i <= n; i++) {
+                while (n!=i){
+                    if(n%i!=0)
+                        break;
+                    factors.add(Integer.valueOf(i));
+                    n=n/i;
+                }
+            }
+            factors.add(Integer.valueOf(n));
+            return factors;
+        }
     }
 }
--- test/com/thoughtworks/primefactors/PrimeFactorsTest.java
+++ test/com/thoughtworks/primefactors/PrimeFactorsTest.java
@@ -2,14 +2,20 @@ package com.thoughtworks.primefactors;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class PrimeFactorsTest {
     @Test
     public void testGenerate() throws Exception {
         PrimeFactors primeFactors=new PrimeFactors();
+        List<Integer> list=new ArrayList<Integer>();
+        list.add(2);
 
         assertEquals(null,primeFactors.generate(1));
+        assertEquals(list,primeFactors.generate(2));
 
     }
 }
